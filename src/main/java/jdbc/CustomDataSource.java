e409ae760b6d87f4e1ac12a6ce4caf65d4656843
Mirabbos7000 <mirabbos488@gmail.com>
2024-07-24 00:42:38 +0500

    Update CustomDataSource.java

--- src/main/java/jdbc/CustomDataSource.java
+++ src/main/java/jdbc/CustomDataSource.java
@@ -5,6 +5,12 @@ import javax.sql.DataSource;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
 @Getter
 @Setter
 public class CustomDataSource implements DataSource {
@@ -15,11 +21,61 @@ public class CustomDataSource implements DataSource {
     private final String password;
 
     private CustomDataSource(String driver, String url, String password, String name) {
+        this.password = password;
+        this.driver = driver;
+        this.name = name;
+        this.url = url;
     }
 
-    public static CustomDataSource getInstance() {
-
+    public static CustomDataSource getInstance(){
+        if (instance == null) {
+            instance = new CustomDataSource();
+        }
         return instance;
     }
 
+    @Override
+    public Connection getConnection() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        System.out.println("NOT SUPPORTED");
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+        System.out.println("NOT SUPPORTED");
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        throw new SQLException("NOT SUPPORTED");
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        throw new SQLException("NOT SUPPORTED");
+    }
 }
